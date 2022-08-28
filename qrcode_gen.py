10a66261e4637274b0dc061b6ea4e70855a01fb7
AbdelKariim <ziani.kerarti.kariim@gmail.com>
2022-08-29 00:21:44 +0100

    first day

--- qrcode_gen.py
+++ qrcode_gen.py
@@ -1,46 +1,68 @@
 import qrcode 
-import os 
 import sqlite3
 
-qr_code = qrcode.QRCode(
-    version = 1,
-    error_correction = qrcode.constants.ERROR_CORRECT_L,
-    box_size = 50,
-    border = 10,
-)
+def init_qrcode():
+    qr_code = qrcode.QRCode(version = 1,
+                    error_correction = qrcode.constants.ERROR_CORRECT_L,
+                    box_size = 50,
+                    border = 10,
+    )
+    return qr_code 
 
-if os.path.exists('product.db'):
-    print("Database exists")
+def connect_db(database_name, timeout=1000):
+    database = sqlite3.connect(database_name, timeout=1000)
+    print("Database {} created".format(database))
+    return database
+    
+def close_db(database_name):
+    database_name.close()
+    
+def create_table(database, table_name):    
+    database.execute(f"CREATE TABLE IF NOT EXISTS {table_name} \
+                        (ID     INT     PRIMARY KEY     NOT NULL, \
+                         NAME   TEXT                    NOT NULL, \
+                         UNIQUE(ID, NAME));")
+    print("Table {} created".format(table_name))
+    database.commit()
+    
+    
+def insert_into_db(database, table_name, values):
+    print(values)
+    columns = ', '.join(values.keys())
+    placeholders = ', '.join('?' * len(values))
+    sql = 'INSERT OR IGNORE INTO {} ({}) VALUES ({})'.format(table_name, columns, placeholders)
+    values = [int(x) if isinstance(x, bool) else x for x in values.values()]
+    database.execute(sql, values)
+    print("Insert done")
+    database.commit()
+    
+def select_from_db(database, table_name, values):
+    values = ', '.join(values)
+    sql = 'SELECT {} from {}'.format(values, table_name)
+    cursor = database.execute(sql)
+    for row in cursor:
+        print("ID = ", row[0])
+        print("NAME = ", row[1])
+        
+        
+        
 
-database = sqlite3.connect('product.db', timeout=1000)
+database = connect_db("product.db")
+    
 print("Connected succesfully !")
 
-database.execute(''' CREATE TABLE IF NOT EXISTS PRODUCT 
-                 (ID     INT     PRIMARY KEY     NOT NULL,
-                 NAME   TEXT                    NOT NULL,
-                 UNIQUE(ID, NAME));
-                 ''')
+create_table(database, "cars")    
 
-database.execute("INSERT OR IGNORE INTO PRODUCT (ID, NAME) \
-                VALUES (1, 'iPhone XS')")
+# insert_into_db(database, 'cars')    
 
-database.execute("INSERT OR IGNORE INTO PRODUCT (ID, NAME) \
-                VALUES (2, 'iPhone Xr')")
+# select_from_db(database, 'product', ['id', 'name'])
 
-database.execute("INSERT OR IGNORE INTO PRODUCT (ID, NAME) \
-                VALUES (3, 'Samsung Galaxy S22 Ultra')")
-
-database.execute("INSERT OR IGNORE INTO PRODUCT (ID, NAME) \
-                VALUES (4, 'Google Pixel 6')")
-
-database.commit()
-print("Record commited successfully")
-
-database.close()
+close_db(database)
 
 
-qr_code.add_data('Some data')
-qr_code.make(fit=True)
+# qr_code = init_qrcode()
+# qr_code.add_data('Some data')
+# qr_code.make(fit=True)
 
-img = qr_code.make_image(fill_color="black", back_color="white")
-img.save('qrcode.png')
+# img = qr_code.make_image(fill_color="black", back_color="white")
+# img.save('qrcode.png')
